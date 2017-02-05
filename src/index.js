0d6814ec44c118f7193d6e7ecd1a6f5d5be62ce5
trungutt <trung.nguyen@adyoulike.com>
2017-02-05 10:07:46 +0100

    chore(redux): setup redux and redux-logger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Heqsdysdse');
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+
+const reducer = (state, action) => {
+	if (action.type === 'INC') {
+		return state + 1;
+	}
+	return state;
+};
+
+const store = createStore(reducer, 0, applyMiddleware(createLogger()));
+
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
