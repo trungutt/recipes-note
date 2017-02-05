18b393f3da2cebff2f8c3152e77ca1965bd43df2
trungutt <trung.nguyen@adyoulike.com>
2017-02-05 09:54:35 +0100

    chore(config): setup eslint airbnb and editorconfig

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('Heysdse');
+console.log('Heqsdysdse');
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,25 @@
 import path from 'path';
 
 export default {
+	devtool: 'inline-source-map',
 	entry: path.join(__dirname, 'src/index'),
 	output: {
 		path: __dirname,
 		filename: 'bundle.js',
-		publicPath: '/'
+		publicPath: '/',
 	},
 	devServer: {
-		contentBase: "./src"
+		contentBase: path.resolve(__dirname, 'src'),
+		quiet: false,
+		noInfo: false,
+		stats: {
+			assets: false,
+			colors: true,
+			version: false,
+			hash: false,
+			timings: false,
+			chunks: false,
+			chunkModules: false,
+		},
 	},
-}
+};
